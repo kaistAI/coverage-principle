Fix: Parallel causal tracing & MRR for Composition task

causal_tracing_composition.py
@@ -17,15 +17,12 @@ def main():
     
     parser = argparse.ArgumentParser()
 
-    # parser.add_argument("--dataset", default=None, type=str, required=True, help="dataset name")
     parser.add_argument("--model_dir", default=None, type=str, help="parent directory of saved model checkpoints")
     parser.add_argument("--step_list", default=None, nargs="+", help="checkpoint's steps to check causal strength")
     parser.add_argument("--save_path", default=None, type=str, help="path to save result")
 
     parser.add_argument("--num_layer", default=8, type=int, help="number of layer of the model")
     parser.add_argument("--target_layer", default=None, type=int, help="Target layer number for checking causal tracing")
-    # parser.add_argument("--wd", default=0.1, type=float, help="weight decay being used")
-    # parser.add_argument("--seed", default=0, type=int, help="seed for numpy random")
     parser.add_argument("--data_type", default="train_inferred", type=str, help="type of inference data (atomic_id, atomic_ood, train_inferred, test_inferred_iid, test_inferred_ood, ...)")
     
     args = parser.parse_args()
@@ -39,12 +36,19 @@ def main():
         dataset = args.model_dir.split("/")[-1].split("_")[0]
 
     device = torch.device('cuda:0')
+    np.random.seed(0)
+    random.seed(0)
 
     all_atomic = set()     # (h,r,t)
     atomic_dict = dict()   # (h,r) -> t
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", dataset, "train.json")) as f:
-        train_items = json.load(f)
-    for item in tqdm(train_items):
+    if "inf" in dataset:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", dataset, "atomic_facts.json")) as f:
+            atomic_items = json.load(f)
+    else:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", dataset, "train.json")) as f:
+            atomic_items = json.load(f)
+ 
+    for item in tqdm(atomic_items):
         temp = item['target_text'].strip("><").split("><")
         if len(temp) != 4:
             continue
@@ -53,6 +57,9 @@ def main():
         all_atomic.add((h,r,t))
 
     id_atomic = set()
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", dataset, "train.json")) as f:
+        train_items = json.load(f)
+
     for item in tqdm(train_items):
         temp = item['target_text'].strip("><").split("><")
         if len(temp) == 4:
@@ -74,6 +81,7 @@ def main():
 
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", dataset, "test.json")) as f:
         pred_data = json.load(f)
+    
     d = dict()
     for item in pred_data:
         t = item['type']
@@ -81,7 +89,7 @@ def main():
             d[t] = []
         d[t].append(item)
 
-    def return_rank(hd, word_embedding_, token, metric='dot', token_list=None):
+    def return_rank(hd, word_embedding_, token_ids_list, metric='dot', token_list=None):
         if metric == 'dot':
             word_embedding = word_embedding_
         elif metric == 'cos':
@@ -90,33 +98,27 @@ def main():
             assert False
 
         logits_ = torch.matmul(hd, word_embedding.T)
+        batch_size, seq_len, vocab_size = logits_.shape[0], logits_.shape[1], logits_.shape[2]
+        
+        token_ids_list = torch.tensor(token_ids_list).view(batch_size, 1, 1).expand(batch_size, seq_len, vocab_size).to(logits_.device) 
+    
+        _, sorted_indices = logits_.sort(dim=-1, descending=True)
+        rank = (sorted_indices == token_ids_list).nonzero(as_tuple=True)[-1].view(batch_size, seq_len).cpu()
 
-        rank = [] 
-        for j in range(len(logits_)):
-            log = logits_[j].cpu().numpy()
-            if token_list is None:
-                temp = [[i, log[i]] for i in range(len(log))]
-            else:
-                temp = [[i, log[i]] for i in token_list]
-            temp.sort(key=lambda var: var[1], reverse=True)
-            rank.append([var[0] for var in temp].index(token))
         return rank
 
-    # if args.step_list == None:
     all_checkpoints = [checkpoint for checkpoint in os.listdir(args.model_dir) if checkpoint.startswith("checkpoint")]
-    # else:
-    #     all_checkpoints = [f"checkpoint-{checkpoint}" for checkpoint in args.step_list]
     assert all(os.path.isdir(os.path.join(args.model_dir, checkpoint)) for checkpoint in all_checkpoints)
     all_checkpoints.sort(key=lambda var: int(var.split("-")[1]))
 
     results = {}
-
-    np.random.seed(0)
+    
     split = args.data_type
-    rand_inds = np.random.choice(len(d[split]), 300, replace=False).tolist()
-
+    # rand_inds = np.random.choice(len(d[split]), 300, replace=False).tolist()
     target_layer = args.target_layer
     total_layer = args.num_layer
+    
+    BATCH_SIZE = 4096
 
     for checkpoint in tqdm(all_checkpoints):
         print("now checkpoint", checkpoint)
@@ -130,70 +132,78 @@ def main():
         model.config.pad_token_id = model.config.eos_token_id
 
         full_list = []
-        for index in tqdm(rand_inds):
-
-            random.seed(index)
+        for i in range(0, len(d[split]), BATCH_SIZE):
+            batch = d[split][i:i+BATCH_SIZE]
+            batch_size = len(batch)
+            query_list = [data['input_text'] for data in batch]
+            temp_dict = dict()
             
-            res_dict = dict()
-
-            query = d[split][index]['input_text']
-            h,n_r,r = query.strip("><").split("><")
-            b = atomic_dict[(h, n_r)]
-            t = atomic_dict[(b, r)]
-
-            decoder_temp = tokenizer([query], return_tensors="pt", padding=True)
-            decoder_input_ids, decoder_attention_mask = decoder_temp["input_ids"], decoder_temp["attention_mask"]
-            decoder_input_ids, decoder_attention_mask = decoder_input_ids.to(device), decoder_attention_mask.to(device)
+            real_h_r1_r2_list = [query.strip("><").split("><") for query in query_list]
+            real_b_list, real_t_list, real_r2_list = [], [], []
+            for h, r1, r2 in real_h_r1_r2_list:
+                b = atomic_dict[(h, r1)]
+                real_b_list.append(b)
+                real_r2_list.append(r2)
+                real_t_list.append(atomic_dict[(b, r2)])
+            
+            tokenizer_output = tokenizer(query_list, return_tensors="pt", padding=True)
+            input_ids, attention_mask = tokenizer_output["input_ids"], tokenizer_output["attention_mask"]
+            input_ids, attention_mask = input_ids.to(device), attention_mask.to(device)
             
             with torch.no_grad():
                 outputs = model(
-                    input_ids=decoder_input_ids,
-                    attention_mask=decoder_attention_mask,
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
                     output_hidden_states=True
                 )
-            all_hidden_states = outputs['hidden_states'] 
-
-            rank_before = return_rank(all_hidden_states[target_layer][0, :, :], word_embedding, tokenizer("<"+t+">")['input_ids'][0])[-1]
-            res_dict['rank_before'] = rank_before
-
+            all_hidden_states = outputs['hidden_states']
+            
+            rank_before = return_rank(all_hidden_states[target_layer], word_embedding, tokenizer([f"<{target}>" for target in real_t_list])["input_ids"])[:, -1].tolist()
+            temp_dict['rank_before'] = rank_before
+            
             # MRRs
             for layer_ind in range(1, total_layer):
                 hidden_states_orig = all_hidden_states[layer_ind]
                 with torch.no_grad():
                     temp = model.transformer.ln_f(hidden_states_orig)
-                res_dict['b_rank_pos1_'+str(layer_ind)] = return_rank(temp[0, :, :], word_embedding, tokenizer("<"+b+">")['input_ids'][0])[1]
-                res_dict['r2_rank_pos2_'+str(layer_ind)] = return_rank(temp[0, :, :], word_embedding, tokenizer("<"+r+">")['input_ids'][0])[2]
+                    
+                temp_dict['b_rank_pos1_'+str(layer_ind)] = return_rank(temp, word_embedding, tokenizer([f"<{target}>" for target in real_b_list])["input_ids"])[:, 1].tolist()
+                temp_dict['r2_rank_pos2_'+str(layer_ind)] = return_rank(temp, word_embedding, tokenizer([f"<{target}>" for target in real_r2_list])["input_ids"])[:, 2].tolist()
             
             if checkpoint in [f"checkpoint-{step}" for step in args.step_list]:
                 # perturb the head entity
-                all_ = set()
-                assert (h, n_r) in atomic_dict
-                for head in h2rt_train:
-                    if (head, n_r) not in atomic_dict:
-                        all_.add(head)
-                        continue
-                    tail = atomic_dict[(head, n_r)]
-                    if (tail, r) not in atomic_dict or atomic_dict[(tail, r)] != t:
-                        all_.add(head)
-                query = "<{}>".format(random.choice(list(all_)))
-
-                decoder_temp = tokenizer([query], return_tensors="pt", padding=True)
-                decoder_input_ids_, decoder_attention_mask = decoder_temp["input_ids"], decoder_temp["attention_mask"]
-                decoder_input_ids_, decoder_attention_mask = decoder_input_ids_.to(device), decoder_attention_mask.to(device)
-
+                all_list = [set() for _ in range(batch_size)]
+                for i, (h, r1, r2) in enumerate(real_h_r1_r2_list):
+                    assert (h, r1) in atomic_dict
+                    for head in h2rt_train:
+                        if (head, r1) not in atomic_dict:
+                            all_list[i].add(head)
+                            continue
+                        bridge = atomic_dict[(head, r1)]
+                        # tail = atomic_dict[(real_b_list[i], r2)]
+                        if (bridge, r2) not in atomic_dict or atomic_dict[(bridge, r2)] != real_t_list[i]:
+                            all_list[i].add(head)
+                query_list = []
+                for i in range(batch_size):
+                    query_list.append(f'<{random.choice(sorted(list(all_list[i]), key=lambda x : int(x.strip("><").split("_")[-1])))}>')
+                
+                tokenizer_output = tokenizer(query_list, return_tensors="pt", padding=True)
+                input_ids_, attention_mask = tokenizer_output["input_ids"], tokenizer_output["attention_mask"]
+                input_ids_, attention_mask = input_ids_.to(device), attention_mask.to(device)
+                
                 with torch.no_grad():
                     outputs_ctft = model(
-                        input_ids=decoder_input_ids_,
-                        attention_mask=decoder_attention_mask,
+                        input_ids=input_ids_,
+                        attention_mask=attention_mask,
                         output_hidden_states=True
                     )
                 all_hidden_states_ctft = outputs_ctft['hidden_states']
-
+                
                 for layer_to_intervene in range(1, target_layer):
                     hidden_states = all_hidden_states[layer_to_intervene].clone()
                     hidden_states_ctft = all_hidden_states_ctft[layer_to_intervene]
                     # intervene
-                    hidden_states[0, 0, :] = hidden_states_ctft[0, 0, :]
+                    hidden_states[:, 0, :] = hidden_states_ctft[:, 0, :]
 
                     with torch.no_grad():
                         for i in range(layer_to_intervene, target_layer):
@@ -211,24 +221,26 @@ def main():
                         # final ln
                         hidden_states = model.transformer.ln_f(hidden_states)
                     # print("--------")
-                    rank_after = return_rank(hidden_states[0, :, :], word_embedding, tokenizer("<"+t+">")['input_ids'][0])[-1]
-                    res_dict['h_'+str(layer_to_intervene)] = rank_after
+                    rank_after = return_rank(hidden_states, word_embedding, tokenizer([f"<{target}>" for target in real_t_list])["input_ids"])[:, -1].tolist()
+                    temp_dict['h_'+str(layer_to_intervene)] = rank_after
                 
                 # perturb the 1st relation
-                all_ = set()
-                rt_list = h2rt_train[h]
-                for (relation, tail) in rt_list:
-                    if (tail, r) not in atomic_dict or atomic_dict[(tail, r)] != t:
-                        all_.add(relation)
-                query = "<{}><{}>".format(h, random.choice(list(all_)))
+                all_list = [set() for _ in range(batch_size)]
+                query_list = []
+                for i, (h, r1, r2) in enumerate(real_h_r1_r2_list):
+                    rt_list = h2rt_train[h]
+                    for (relation, bridge) in rt_list:
+                        if (bridge, r2) not in atomic_dict or atomic_dict[(bridge, r2)] != real_t_list[i]:
+                            all_list[i].add(relation)
+                    query_list.append(f"<{h}><{random.choice(list(all_list[i]))}>")
 
-                decoder_temp = tokenizer([query], return_tensors="pt", padding=True)
-                decoder_input_ids_, decoder_attention_mask = decoder_temp["input_ids"], decoder_temp["attention_mask"]
-                decoder_input_ids_, decoder_attention_mask = decoder_input_ids_.to(device), decoder_attention_mask.to(device)
+                tokenizer_output = tokenizer(query_list, return_tensors="pt", padding=True)
+                input_ids_, attention_mask = tokenizer_output["input_ids"], tokenizer_output["attention_mask"]
+                input_ids_, attention_mask = input_ids_.to(device), attention_mask.to(device)
                 with torch.no_grad():
                     outputs_ctft = model(
-                        input_ids=decoder_input_ids_,
-                        attention_mask=decoder_attention_mask,
+                        input_ids=input_ids_,
+                        attention_mask=attention_mask,
                         output_hidden_states=True
                     )
                 all_hidden_states_ctft = outputs_ctft['hidden_states']
@@ -237,7 +249,7 @@ def main():
                     hidden_states = all_hidden_states[layer_to_intervene].clone()
                     hidden_states_ctft = all_hidden_states_ctft[layer_to_intervene]
                     # intervene
-                    hidden_states[0, 1, :] = hidden_states_ctft[0, 1, :]
+                    hidden_states[:, 1, :] = hidden_states_ctft[:, 1, :]
 
                     with torch.no_grad():
                         for i in range(layer_to_intervene, target_layer):
@@ -255,19 +267,24 @@ def main():
                         # final ln
                         hidden_states = model.transformer.ln_f(hidden_states)
                     # print("--------")
-                    rank_after = return_rank(hidden_states[0, :, :], word_embedding, tokenizer("<"+t+">")['input_ids'][0])[-1]
-                    res_dict['r1_'+str(layer_to_intervene)] = rank_after
+                    rank_after = return_rank(hidden_states, word_embedding, tokenizer([f"<{target}>" for target in real_t_list])["input_ids"])[:, -1].tolist()
+                    temp_dict['r1_'+str(layer_to_intervene)] = rank_after
+                # print(temp_dict)
                 
                 # perturb the second relation
-                all_ = set()
-                rt_list = h2rt_train[b]
-                for (relation, tail) in rt_list:
-                    if tail != t:
-                        assert relation != r
-                        all_.add(relation)
-                query = "<{}><{}><{}>".format(h, n_r, random.choice(list(all_)))
+                all_list = [set() for _ in range(batch_size)]
+                query_list = []
+                for i, (h, r1, r2) in enumerate(real_h_r1_r2_list):
+                    b = real_b_list[i]
+                    rt_list = h2rt_train[b]
+                    t = real_t_list[i]
+                    for (relation, tail) in rt_list:
+                        if tail != t:
+                            assert relation != r2
+                            all_list[i].add(relation)
+                    query_list.append(f"<{h}><{r1}><{random.choice(list(all_list[i]))}>")
 
-                decoder_temp = tokenizer([query], return_tensors="pt", padding=True)
+                decoder_temp = tokenizer(query_list, return_tensors="pt", padding=True)
                 decoder_input_ids_, decoder_attention_mask = decoder_temp["input_ids"], decoder_temp["attention_mask"]
                 decoder_input_ids_, decoder_attention_mask = decoder_input_ids_.to(device), decoder_attention_mask.to(device)
 
@@ -283,7 +300,7 @@ def main():
                     hidden_states = all_hidden_states[layer_to_intervene].clone()
                     hidden_states_ctft = all_hidden_states_ctft[layer_to_intervene]
                     # intervene
-                    hidden_states[0, 2, :] = hidden_states_ctft[0, 2, :]
+                    hidden_states[:, 2, :] = hidden_states_ctft[:, 2, :]
 
                     with torch.no_grad():
                         for i in range(layer_to_intervene, target_layer):
@@ -300,11 +317,16 @@ def main():
                             hidden_states = residual + feed_forward_hidden_states
                         # final ln
                         hidden_states = model.transformer.ln_f(hidden_states)
-                    rank_after = return_rank(hidden_states[0, :, :], word_embedding, tokenizer("<"+t+">")['input_ids'][0])[-1]
-                    res_dict['r2_'+str(layer_to_intervene)] = rank_after
-
-            full_list.append(res_dict)
-        
+                    rank_after = return_rank(hidden_states, word_embedding, tokenizer([f"<{target}>" for target in real_t_list])["input_ids"])[:, -1].tolist()
+                    temp_dict['r2_'+str(layer_to_intervene)] = rank_after
+            
+            result_dict_list = [dict() for _ in range(batch_size)]
+            for key, value_list in temp_dict.items():
+                for i in range(batch_size):
+                    result_dict_list[i][key] = value_list[i]
+                    
+            full_list = full_list + result_dict_list
+            
         results[checkpoint] = full_list
 
     with open(os.path.join(args.save_path, f"{args.model_dir.split('/')[-1]}-{args.data_type}.json"), "w", encoding='utf-8') as f: