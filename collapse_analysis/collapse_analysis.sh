5507de85bfe212a82bb0928cac8d8726b8fa8270
Bread0288 <binlepain178@gmail.com>
2025-02-06 17:17:05 +0900

    Add: option for combining ID_Train & ID_Test in collapse analysis

--- collapse_analysis/collapse_analysis.sh
+++ collapse_analysis/collapse_analysis.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-# STEP=250
-# CHECKPOINT_DIR="/mnt/nas/jinho/GrokkedTransformer/trained_checkpoints/composition.2000.200.inf-controlled_wd-0.1_layer-8_head-12_seed-42/checkpoint-${STEP}"
 
-# for step in 250
 for step in 250 3500 30000 300000
 do
     CHECKPOINT_DIR="/mnt/nas/jinho/GrokkedTransformer/trained_checkpoints/composition.2000.200.inf-controlled_wd-0.1_layer-8_head-12_seed-42/checkpoint-${step}"
-    python collapse_analysis_copy.py \
+    python collapse_analysis.py \
         --ckpt ${CHECKPOINT_DIR}/ \
         --layer_pos_pairs "[(5,1)]" \
-        --save_dir /mnt/nas/jinho/GrokkedTransformer/collapse_analysis
+        --save_dir /mnt/nas/jinho/GrokkedTransformer/collapse_analysis \
+        --merge_id_data
 done
-    # --id_train_save_fname id_train_${step}_dedup.json \
-    # --id_test_save_fname id_test_${step}_dedup.json \
-    # --ood_save_fname ood_${step}_dedup.json \
-    # --nonsense_save_fname nonsense_${step}_dedup.json
--- collapse_analysis/measure_metrics.sh
+++ collapse_analysis/measure_metrics.sh
@@ -1,10 +1,11 @@
-DIR='/mnt/nas/jinho/GrokkedTransformer/collapse_analysis/composition.2000.200.inf-controlled/test/(5,1)'
+DIR='/mnt/nas/jinho/GrokkedTransformer/collapse_analysis/composition.2000.200.inf-controlled/merged_id/(5,1)'
 
 for step in 250 3500 30000 300000
 do
-    python measure_metrics_copy.py \
-        --id_train_file ${DIR}/${step}/id_dedup.json \
+    python measure_metrics.py \
+        --id_train_file ${DIR}/${step}/id_merged_dedup.json \
+        --id_test_file ${DIR}/${step}/id_merged_dedup.json \
         --ood_file ${DIR}/${step}/ood_dedup.json \
         --nonsense_file ${DIR}/${step}/nonsense_dedup.json \
-        --output_dir "/mnt/nas/jinho/GrokkedTransformer/collapse_analysis/results/composition.2000.200.inf-controlled/test/(5,1)/step${step}"
+        --output_dir "/mnt/nas/jinho/GrokkedTransformer/collapse_analysis/results/composition.2000.200.inf-controlled/merged_id/(5,1)/step${step}"
 done
