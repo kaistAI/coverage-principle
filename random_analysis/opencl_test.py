75b52e931a1e2f8f4520085657ad93a1dba53ff1
duemoo <retapurayo@snu.ac.kr>
2025-05-22 15:24:57 +0900

    remove files

--- random_analysis/opencl_test.py
+++
@@ -1,8 +0,0 @@
-import pyopencl as cl
-
-platforms = cl.get_platforms()
-for platform in platforms:
-    print("Platform:", platform.name)
-    devices = platform.get_devices()
-    for device in devices:
-        print("  Device:", device.name, "- Type:", cl.device_type.to_string(device.type))
