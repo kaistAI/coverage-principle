ddccc22d49eb838203c03058512ca6051eac46c0
duemoo <retapurayo@snu.ac.kr>
2025-05-22 15:30:04 +0900

    remove files

--- analysis.sh
+++
@@ -1,8 +0,0 @@
-step=400000
-
-python analysis/analysis.py \
-  --dataset analysis/results/raw/composition_dedup_${step}.json \
-  --layer_pos_pairs "[(0,0), (0,1), (5,0), (5,1), (5,2), (8,0), (8,1), (8,2)]" \
-  --save_dir analysis/results/${step} \
-  --save_fname composition_${step}.json \
-  --model_path checkpoints/composition/composition.2000.200.9.0_0.1_8/checkpoint-${step}/
